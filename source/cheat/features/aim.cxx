7ad09b0c60bfe08a55bbb6c82d0a7ec44d028f44
Samuel <181611094+svespalec@users.noreply.github.com>
2026-01-02 15:16:23 -0800

    add update gameplay state hook and cleanup

--- source/cheat/features/aim.cxx
+++ source/cheat/features/aim.cxx
@@ -5,31 +5,53 @@
 #include <sdk/sdk.hxx>
 
 namespace features::aim {
-  using apply_input_t = void( __fastcall* )( void*, void*, void* );
-  static apply_input_t original = nullptr;
+  static player* g_player = nullptr;
 
-  void __fastcall hooked_apply_input( void* thisptr, void* state, void* handler ) {
-    original( thisptr, state, handler );
+  using apply_input_t = void( __fastcall* )( void*, input_state*, void* );
+  static apply_input_t original_apply_input = nullptr;
 
-    // TODO: implement aim logic
-    // if ( !g_state.in_gameplay )
-    //   return;
-    //
+  using update_state_t = void( __fastcall* )( player* );
+  static update_state_t original_update_gameplay_state = nullptr;
+
+  void __fastcall hooked_update_gameplay_state( player* local ) {
+    g_player = local;
+
+    original_update_gameplay_state( local );
+  }
+
+  void __fastcall hooked_apply_input( void* thisptr, input_state* state, void* handler ) {
+    original_apply_input( thisptr, state, handler );
+
+    if ( !g_player )
+      return;
+
+    if ( !g_player->is_playing() )
+      return;
+
     // state->mouse->position = process( state->mouse->position );
   }
 
   constexpr auto mouse_apply_fn = "57 56 55 53 48 83 EC 48 C5 F8 77 C5 F8 29 74 24 30 C5 F8 29 7C 24 20 48 8B";
+  constexpr auto update_gameplay_state_fn = "56 53 48 83 EC 28 48 8B D9 48 8B 8B 58 04 00 00 48 8B 89 10 03 00 00";
 
   void init() {
     void* mouse_apply = nullptr;
+    void* update_gameplay = nullptr;
 
-    while ( !mouse_apply ) {
-      mouse_apply = utils::find_pattern( mouse_apply_fn );
+    while ( !mouse_apply || !update_gameplay ) {
+      if ( !mouse_apply )
+        mouse_apply = utils::find_pattern( mouse_apply_fn );
+
+      if ( !update_gameplay )
+        update_gameplay = utils::find_pattern( update_gameplay_state_fn );
+
       Sleep( 500 );
     }
 
     LOG( "aim: found mouse_apply @ %p", mouse_apply );
+    LOG( "aim: found update_gameplay @ %p", update_gameplay );
 
-    SETUP_HOOK( mouse_apply, hooked_apply_input, original );
+    SETUP_HOOK( mouse_apply, hooked_apply_input, original_apply_input );
+    SETUP_HOOK( update_gameplay, hooked_update_gameplay_state, original_update_gameplay_state );
   }
 } // namespace features::aim
