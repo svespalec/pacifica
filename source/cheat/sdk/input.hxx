bdb15d0a96eaa3ce99c885672ebfcacc707b7c72
Samuel <181611094+svespalec@users.noreply.github.com>
2026-01-02 02:01:47 -0800

    input sdk impl

--- source/cheat/sdk/input.hxx
+++ source/cheat/sdk/input.hxx
@@ -1,3 +1,29 @@
 #pragma once
 
 #include "vec2.hxx"
+
+struct mouse_state {
+  void* method_table;
+  void* buttons;
+  void* last_source;
+  bool is_position_valid;
+  char _pad1[ 0x3 ];
+  vec2 scroll;
+  vec2 position;
+  char _pad2[ 0xC ];
+};
+
+struct keyboard_state {
+  void* method_table;
+  void* keys;
+};
+
+struct input_state {
+  void* method_table;
+  mouse_state* mouse;
+  keyboard_state* keyboard;
+  void* touch;
+  void* joystick;
+  void* midi;
+  void* tablet;
+};
